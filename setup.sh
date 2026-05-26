113f05044d35cb54aedb3477488b5fabc9a1bda7
Marlo Rodrigues <marloatma8@gmail.com>
2026-05-26 18:08:38 -0300

    added redis docker run on docs

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ docker run --name me-postgres -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=pos
 
 docker run --name me-influx -p 8086:8086 -e DOCKER_INFLUXDB_INIT_MODE=setup -e DOCKER_INFLUXDB_INIT_USERNAME=influx -e DOCKER_INFLUXDB_INIT_PASSWORD=influx -e DOCKER_INFLUXDB_INIT_ORG=moonexchange -e DOCKER_INFLUXDB_INIT_BUCKET=me-bucket -d influxdb:1.12-alpine;
 
-
+docker run --name redis -p 6379:6379 -d redis;
 
 
 
